b5184a578bb46bba4fbff6ab94e9ed6e7e7a204c
deryadenizballi <dderyaaballi@gmail.com>
2021-11-20 11:08:04 +0300

    user page added

---
+++ app/script.js
@@ -0,0 +1,30 @@
+const warContainer=document.querySelector(".war__container");
+const loginContainer=document.querySelector(".login__container");
+const btnWar=document.querySelector(".btn-war");
+const btnAdd=document.querySelector(".btn-add");
+const wordContainer=document.querySelector(".word__container");
+
+btnWar.addEventListener('click', addwar());
+btnAdd.addEventListener('click',addword());
+
+function addwar(){
+    if(warContainer.style.display=="none"){
+        warContainer.style.display="block";
+        loginContainer.style.display="none";
+    }
+    else{
+        warContainer.style.display="none";
+        loginContainer.style.display="block";
+    }
+}
+
+function addword(){
+    if(wordContainer.style.display=="none"){
+        wordContainer.style.display="block";
+        loginContainer.style.display="none";
+    }
+    else {
+        wordContainer.style.display="none";
+        loginContainer.style.display="block";
+    }
+}
