f315f4c53426c59554c8720951b559c0770c59c3
deryadenizballi <dderyaaballi@gmail.com>
2021-11-28 22:57:17 +0300

    preloading add

--- app/script.js
+++ app/script.js
@@ -1,3 +1,14 @@
+// prelodaing
+// var loader = document.querySelector(".preloader");
+
+// window.addEventListener("load", function () {
+//   setTimeout(()=>{
+//     loader.style.display = "none";
+//   },3000)
+
+// });
+
+
 const warContainer = document.querySelector(".war__container");
 const loginContainer = document.querySelector(".login__container");
 const btnWar = document.querySelector(".btn-war");
@@ -5,14 +16,14 @@ const btnAdd = document.querySelector(".btn-add");
 const wordContainer = document.querySelector(".word__container");
 const counter = document.querySelector(".counter");
 const btnStart = document.querySelector(".btn-start");
-console.log(btnStart);
+const imgName=document.querySelector(".img_name");
+console.log(imgName);
 
 // olay dinleyicileri
 btnWar.addEventListener("click", addwar());
 btnAdd.addEventListener("click", addword());
 
-// zaman sayacı ekle 
-
+// zaman sayacı ekle
 
 function addwar() {
   if (warContainer.style.display == "none") {
@@ -33,4 +44,5 @@ function addword() {
     loginContainer.style.display = "block";
   }
 }
+
 
